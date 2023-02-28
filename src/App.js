5b27051d86bd56e596d74c4d0664280dc135edeb
Nguyen Van Qui <vanquyjr11@gmail.com>
2023-02-28 22:47:27 +0700

    update font Barlow

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Header from "./header/Header";
 import Item from "./item/Item";
 import ItemList from "./item/ItemList";
 import { fakeData } from "./item/fakeData";
+import { GlobalStyles } from "./GlobalStyles";
 
 function App() {
   return (
     <div>
+      <GlobalStyles></GlobalStyles>
       <Header></Header>
       <ItemList>
         {fakeData.map((item, index) => (
---
+++ src/GlobalClasses.js
@@ -0,0 +1,8 @@
+import { css } from "styled-components";
+
+export const GlobalClasses = css`
+  @import url("https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;700&display=swap");
+  body {
+    font-family: "Barlow", sans-serif;
+  }
+`;
